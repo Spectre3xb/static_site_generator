e51c0124971cd773e6372ba8ab94218ba4fc0da1
Spectre3xb <stefan.g.buchholz@gmail.com>
2025-10-27 22:14:57 +0100

    getting started

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+#print("hello world")
+
+def main():
+    test_node = TextNode("Some arbitrary text", "link", "https://www.google.com")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
